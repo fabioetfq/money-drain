af16dbd2dde99f57bd663837d86e48bee2c8b0c9
Fabio Oliveira <fabio.oliveira@agricultura.gov.br>
2022-01-05 22:23:39 -0300

    Item CRUD

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,45 @@
 class ItemsController < ApplicationController
+  before_action :find, only: [:show, :edit, :update, :delete]
+  def index
+    @items = Item.all
+  end
+
+  def show
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @item.update(item_params)
+    redirect_to root_path
+  end
+
+  def delete
+    @item.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :category)
+  end
+
+  def find
+    @item = Item.find(params[:id])
+  end
 end
